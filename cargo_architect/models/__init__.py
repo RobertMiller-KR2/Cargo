64f9fd5b6a003aa218919eba07003afae6c3e11b
Robert Miller <111068606+RobertMiller-KR2@users.noreply.github.com>
2026-06-10 14:50:02 -0500

    Delete cargo_architect directory

--- cargo_architect/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import cargo_trailer_preset
-from . import cargo_product_preset
-from . import cargo_branding_profile
-from . import cargo_load_plan
-from . import cargo_load_line
-from . import cargo_placement
-
-from . import res_config_settings
